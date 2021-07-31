525cf651d307d142046cdce648bf5e61f12f4ae6
LeeKangh22 <dlrkdgml1998@dongguk.edu>
2021-07-31 17:42:20 +0900

    7/31 solved by Leekh

--- Level1/완주하지못한선수.py
+++
@@ -1,9 +0,0 @@
-def solution(participant, completion):
-    answer = ''
-    participant.sort()
-    completion.sort()
-    for i in range(len(completion)):
-        if participant[i]!=completion[i]:
-            return participant[i]
-            break
-    return participant[i + 1]
