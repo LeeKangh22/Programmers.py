d3c8f06ff640c44c8414be3d655efc44feab2cbe
LeeKangh22 <dlrkdgml1998@dongguk.edu>
2021-08-03 18:36:29 +0900

    8/3 solved by Leekh

---
+++ Level1/비밀지도.py
@@ -0,0 +1,25 @@
+
+def solution(n, arr1, arr2):
+    answer = []
+    
+    for i in range(0, len(arr1)):
+        string = ''
+        arr1[i] = bin(arr1[i])
+        for j in range(0, n - (len(arr1[i]) - 2)):
+            string = string + '0'
+        arr1[i] = string + arr1[i][2:]
+    for i in range(0, len(arr2)):
+        string = ''
+        arr2[i] = bin(arr2[i])
+        for j in range(0, n - (len(arr2[i]) - 2)):
+            string = string + '0'
+        arr2[i] = string + arr2[i][2:]
+    for i in range(0, n):
+        string = ''
+        for j in range(0, n):
+            if arr1[i][j] == '0' and arr2[i][j] == '0':
+                string += ' '
+            else:
+                string += '#'
+        answer.append(string)
+    return answer
