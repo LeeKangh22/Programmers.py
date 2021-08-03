251e7ce60575551d40a39d6b424ec39231a21e5d
LeeKangh22 <dlrkdgml1998@dongguk.edu>
2021-08-03 20:23:39 +0900

    8/3 solved by Leekh, for문 인덱싱 내용 확인

--- Level1/다트게임.py
+++ Level1/다트게임.py
@@ -1,12 +1,12 @@
 dartResult = "1S2D*3T"
 answer = 0
 def solution(dartResult):
-    dartResult += ' '
+    dartResult += ' ' # index error 방지를 위해 삽입, 애초에 접근 자체가 안된다.
     answer = 0
     before = 0
     score = 0
     i = 0
-    while i < len(dartResult):
+    while i < len(dartResult): # 파이썬에서는 for문 index 변경이 불가능하다. 따라서 While문을 이용한 indexing을 진행
         if dartResult[i] >= '0' and dartResult[i] <= '9':
             before = score
             if dartResult[i + 1] == '0':
