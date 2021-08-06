a2812affb854bc8215c1bb1859c5af979298d920
LeeKangh22 <dlrkdgml1998@dongguk.edu>
2021-08-06 18:52:56 +0900

    8/6 solving by Leekh

--- Level1/3진법뒤집기.py
+++ Level1/3진법뒤집기.py
@@ -1,12 +1,21 @@
-n=7
+def reverse(str):
+    temp = ''
+    for i in range(0, len(str) / 2):
+        temp = str[i]
+        str[i] = str[len(str) - i]
+        str[len(str) - i] = temp
+
+def three(n, str):
+    i = 0
+    while pow(3, i) >= n:
+        i += 1
+    str += ? # 해결중
+    return n % pow(3, i)
 
 def solution(n):
     answer = 0
-    three = int(str(n),3)
-    temp = ''
-    for i in range(0, 2):
-        three[i] = three[len(three) - 1 - i]
-    answer = int(three, 10)    
+
+    while three(n)
+    
 
     return answer
-print(solution(n))
