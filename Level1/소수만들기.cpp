6f18f3ab83d2485fbca36b69b9d25d6f1ff87200
LeeKangh22 <dlrkdgml1998@dongguk.edu>
2021-08-07 17:42:37 +0900

    8/7 solved by Leekh

---
+++ Level1/소수만들기.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+bool isPrime(int sum) { 
+	for (int i = 2; i = sqrt(sum); i++) {
+		if ((sum % i)) == 0)
+			return false;
+	}
+	return true;
+}
+
+int solution(vector<int> nums) {
+
+	int answer = 0;
+
+	for (int i = 0; i <= nums.size() - 3; i++) {
+		for (int j = i; j <= nums.size() - 2; j++) {
+			for (int k = j; k <= nums.size(); k++) {
+				if (isPrime(nums[i] + nums[j] + nums[k]))
+					answer++;
+			}
+		}
+	}
+	
+	return answer;
+}
