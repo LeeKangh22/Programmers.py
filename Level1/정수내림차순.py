a0eff79bb0adf044a95a4000f3e844752add7584
LeeKangh22 <dlrkdgml1998@dongguk.edu>
2021-08-08 15:10:06 +0900

    8/8 solved by Leekh

---
+++ Level1/정수내림차순.py
@@ -0,0 +1,14 @@
+def solution(n):
+    answer = 0
+    string = str(n)
+    temp = []
+    for i in range(0, len(string)):
+        temp.append(int(string[i]))
+    temp.sort(reverse = True)
+    string2 = ''
+    for i in range(0, len(string)):
+        temp[i] = str(temp[i])
+        string2 += temp[i]
+    answer = int(string2)
+        
+    return answer
