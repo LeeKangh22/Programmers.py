cacd480747a095388f20c2c79402981cf165c65b
LeeKangh22 <dlrkdgml1998@dongguk.edu>
2021-08-15 17:08:36 +0900

    8/12 solving by Leekh

---
+++ Level2/가장 큰 수.py
@@ -0,0 +1,12 @@
+def solution(numbers):
+    answer = ''
+    start = 0
+    sum = ''
+    temp = []
+    for i in range(0, len(numbers)):
+        start = numbers[i]
+        for j in range(0, len(numbers)):
+            if j == i:
+                pass
+            
+    return answer
