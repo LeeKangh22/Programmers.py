295cf67c698d07f3eb6e8de15dd41268bfcca59a
LeeKangh22 <dlrkdgml1998@dongguk.edu>
2021-08-17 21:01:13 +0900

    8/16 solving by Leekh

---
+++ Level2/영어 끝말 잇기.py
@@ -0,0 +1,12 @@
+def solution(n, words):
+    answer = []
+    temp = words[0]
+    position = 0
+    count = 0 # 순서를 체크
+    
+    
+    for i in range(1, len(words)):
+        if temp == words[i]:
+            
+
+    return answer
