c05da5277cd32822e4add933a9343de1cbd6f027
LeeKangh22 <dlrkdgml1998@dongguk.edu>
2021-08-20 18:19:31 +0900

    8/20 solved by Leekh

---
+++ Level1/문자열내림.py
@@ -0,0 +1,12 @@
+s = 'Zbcdefg'
+def solution(s):
+    answer = ''
+    temp = []
+    for i in range(0, len(s)):
+        temp.append(s[i])
+    temp.sort(reverse = True)
+    
+    for i in range(0, len(temp)):
+        answer += temp[i]
+    return answer
+print(solution(s))
