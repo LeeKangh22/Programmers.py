1b3b730cc5658dec22d4ab3381ff81ad3ae54f77
LeeKangh22 <dlrkdgml1998@dongguk.edu>
2021-08-23 16:28:21 +0900

    8/23 solved by Leekh

--- Level2/4주차.py
+++ Level2/4주차.py
@@ -6,6 +6,9 @@ def solution(table, languages, preference):
     temp = [] #각 직업군 별 언어를 공백 기준으로 스플릿 하여 넣어놓을 값
     pref = [] #직업군 별 직업군 언어 점수 * 언어 선호도 곱하여 넣을 값 
     score = 0
+    #테이블에서 한 직업 당 스플릿한다.
+    #이후 언어에서 그 직업의 언어 점수를 수식으로 곱하여 직업당 점수를 계산한다.
+    #이를 모두 저장하고 가장 큰 값을 찾아낸다.
     for i in range(0, len(table)):
         temp = table[i].split()
         for j in range(0, len(temp)):
@@ -22,6 +25,7 @@ def solution(table, languages, preference):
         if max <= pref[i]:
             max = pref[i]
     
+    # 점수가 가장 큰 직업을 다 넣어놓고 사전순으로 정렬하여 가장 처음에 위치한 직업을 출력한다.
     if pref[0] == max:
         lan_temp.append('SI')
     if pref[1] == max:
