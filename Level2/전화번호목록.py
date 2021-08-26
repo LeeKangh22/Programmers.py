0a41db5963b9f939e588c210197659ec1e5bcf3b
LeeKangh22 <dlrkdgml1998@dongguk.edu>
2021-08-26 14:52:36 +0900

    8/26 modified

--- Level2/전화번호목록.py
+++ Level2/전화번호목록.py
@@ -8,16 +8,6 @@ def solution(phone_book): #다시 풀어보기
     for i in range(0, len(phone_book)):
         if temp1 >= len(phone_book[i]):
             temp1 = len(phone_book[i])
-    '''
-    for i in range(0, len(phone_book)):
-        for j in range(0, len(phone_book)):
-            if j == i:
-                pass
-            else:
-                if phone_book[i][:temp1] == phone_book[j][:temp1]:
-                    answer = False
-                    break
-    '''
     
     for i in range(0, len(phone_book)):
         temp_str.append(phone_book[i][:temp1])
