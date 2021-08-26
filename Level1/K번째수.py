9818b7c8715ec783a3c9afd297c3f55e88cc6a9e
LeeKangh22 <dlrkdgml1998@dongguk.edu>
2021-08-26 14:50:07 +0900

    8/26 modified

--- Level1/K번째수.py
+++ Level1/K번째수.py
@@ -1,6 +1,3 @@
-array = [1,5,2,6,3,7,4]
-commands = [[2,5,3],[4,4,1],[1,7,3]]
-
 def solution(array, commands):
     answer = []
     temp = []
@@ -12,6 +9,3 @@ def solution(array, commands):
         temp = []
 
     return answer
-
-answer = solution(array,commands)
-print(answer)
