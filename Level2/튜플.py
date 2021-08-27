4169cef44d2693dd18a3c8964e06c0152f68a48b
LeeKangh22 <dlrkdgml1998@dongguk.edu>
2021-08-27 18:11:42 +0900

    8/27 solving by Leekh

---
+++ Level2/튜플.py
@@ -0,0 +1,17 @@
+s = "{{2},{2,1},{2,1,3},{2,1,3,4}}"
+'''
+,로 튜플로 나눈 다음에 괄호 싹다 없애기?
+아니면 가장 긴 길이의 퓨틀 본 다음에 그걸로 답 내기?
+'''
+
+def solution(s):
+    answer = []
+    temp = []
+    for i in range(0, len(s)):
+        temp.append(s[i])
+    my_set = set(temp)
+    temp = list(my_set)
+    print(temp) 
+    return answer
+
+solution(s)
