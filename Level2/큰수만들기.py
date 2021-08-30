f242ca497d10cfd9a70540affaf9f585964cf213
LeeKangh22 <dlrkdgml1998@dongguk.edu>
2021-08-30 20:55:00 +0900

    8/30 solving by Leekh

--- Level2/큰수만들기.py
+++ Level2/큰수만들기.py
@@ -1,21 +1,20 @@
-import random
-
 number = "1924"
 #solving, 푸는 중
 
 '''
 앞에서부터 k개씩 검사한다. 여기서 가장 작은걸 뺀다.
-이걸 k번 하면 큰 수가 만들어진다. 만약 가장 작은수가 맨 앞에 있으면 맨앞에 것을 뺀다.
+이걸 k번 하면 큰 수가 만들어진다. 만약 가장 작은수가 맨 앞에 있으면 맨앞에 것을 뺀다. --> 알고리즘 틀린 것 같음. 테스트케이스 3개에서만 돌아감 시간초과도 뜸
 '''
 
 def solution(number, k):
     answer = ''
     temp = []
-    num = ''
-    
-    for i in number:
-        for j in range(0, )
-            
+    for j in range(0, len(number)):
+        temp.append(int(number[j]))
+    for j in range(0, k):
+        temp.remove(min(temp[0:k])) 
+    for i in range(0, len(temp)):
+        answer += str(temp[i])
 
     return answer
-solution(number,2)
+print(solution(number,2))
