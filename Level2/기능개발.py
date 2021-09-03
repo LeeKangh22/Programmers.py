bc4bc5a3ac903912258b5878d445242127230a60
LeeKangh22 <dlrkdgml1998@dongguk.edu>
2021-09-03 14:44:27 +0900

    9/3  solved by Leekh

---
+++ Level2/기능개발.py
@@ -0,0 +1,27 @@
+progresses = [95, 90, 99, 99, 80, 99]
+speeds = [1, 1, 1, 1, 1, 1]
+
+def solution(progresses, speeds):
+    answer = []
+    count_stack = []
+    count = 0
+    for i in range(0, len(progresses)):
+        count = 0
+        while progresses[i] < 100:
+            progresses[i] += speeds[i]
+            count += 1
+        count_stack.append(count)
+    print(count_stack)
+    progress = count_stack[0]
+    count = 1
+    for i in range(1, len(count_stack)):
+        if count_stack[i] <= progress:
+            count += 1
+        else:
+            answer.append(count)
+            progress = count_stack[i]
+            count = 1 
+    answer.append(count)        
+            
+    return answer
+print(solution(progresses, speeds))
