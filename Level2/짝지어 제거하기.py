721e6734e869eb5c07d03ba916f39bd697b019d2
LeeKangh22 <dlrkdgml1998@dongguk.edu>
2021-09-05 16:02:47 +0900

    9/5 solving by Leekh

--- Level2/짝지어 제거하기.py
+++ Level2/짝지어 제거하기.py
@@ -1,22 +1,26 @@
 s = 'gbacabbbaagtggtc'
-
+#답은 잘 나옴. 시간초과로 실패가 뜸
 def remove_(s):
+    a = s
     for i in range(0, len(s) - 1):
             temp = s[i]
             if s[i + 1] is temp:
                 a = s.replace(temp + temp,'')
-                return a
-    return s
+    return a
 
 def solution(s):
-    temp = ''
     if len(s) % 2 == 1:
         return 0
     else:
         a = s
-        for i in range(0, int(len(s) / 2)):
+        count = 0
+        while count == 0:
             a = remove_(a)
+            if len(a) == len(remove_(a)):
+                count += 1
             if len(a) == 0:
                 return 1
+            elif count == 1:
+                return 0
     return 0
 print(solution(s))
