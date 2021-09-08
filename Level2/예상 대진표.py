e622a40cf435e607599fc2f12f2322726baab8c4
LeeKangh22 <dlrkdgml1998@dongguk.edu>
2021-09-08 22:15:41 +0900

    9/8 solved by Leekh

---
+++ Level2/예상 대진표.py
@@ -0,0 +1,32 @@
+def solution(n,a,b):
+    answer = 1  
+    if b > a:
+        while True:
+            if b % 2 == 0 and b - a == 1:
+                break
+            else:
+                if a % 2 == 1:
+                    a = (a + 1) / 2
+                else:
+                    a = a / 2
+                if b % 2 == 1:
+                    b = (b + 1) / 2
+                else:
+                    b = b / 2
+            answer += 1
+    else: 
+        while True:
+            if a % 2 == 0 and a - b == 1:
+                break
+            else:
+                if a % 2 == 1:
+                    a = (a + 1) / 2
+                else:
+                    a = a / 2
+                if b % 2 == 1:
+                    b = (b + 1) / 2
+                else:
+                    b = b / 2
+            answer += 1
+
+    return answer
