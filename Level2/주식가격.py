e76c5682ab39439c8c9257330f9cd6760a57f140
LeeKangh22 <dlrkdgml1998@dongguk.edu>
2021-09-08 23:45:13 +0900

    9/8 solved by Leekh

---
+++ Level2/주식가격.py
@@ -0,0 +1,11 @@
+
+def solution(prices):
+    answer = []
+    for i in range(0, len(prices)):
+        for j in range(i, len(prices)):
+            if prices[i] > prices[j] or j == len(prices) - 1:
+                answer.append(j - i)
+                break
+    return answer
+
+
