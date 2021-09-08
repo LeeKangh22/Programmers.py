f7edb8117e26b8805a5886d6679498e00b72852b
LeeKangh22 <dlrkdgml1998@dongguk.edu>
2021-09-08 19:57:00 +0900

    9/8 solved by Leekh

--- Level2/5주차.py
+++ Level2/5주차.py
@@ -1,4 +1,15 @@
+from itertools import *
+word = "AAAAE"
 def solution(word):
     answer = 0
+    n = []
+    for i in range(1, 6):
+        n += list(product("AEIOU", repeat = i))
+    n.sort()
+    str = []
+    for i in range(0, len(n)):
+        str.append("".join(n[i]))
+    answer = str.index(word) + 1
     
     return answer
+print(solution(word))
