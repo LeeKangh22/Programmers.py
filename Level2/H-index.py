058af735b5668505e32b20007928adbcac71bfe4
LeeKangh22 <dlrkdgml1998@dongguk.edu>
2021-09-10 16:35:34 +0900

    9/10 solving by Leekh

---
+++ Level2/H-index.py
@@ -0,0 +1,21 @@
+citations = [3, 0, 6, 1, 5]
+
+def solution(citations):
+    answer = 0
+    temp = []
+    citations.sort()
+    count = 0
+    h_temp = []
+    for i in range(0, len(citations)):
+        print(citations)
+        h = citations[i]
+        count = len(citations)
+        if count < h:
+            break
+        else:
+            h_temp.append(h)
+            citations.pop(0)
+    answer = max(h_temp)
+    
+    return answer
+print(solution(citations))
