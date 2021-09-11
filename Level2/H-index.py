d3765100062470ce9efaf0302c231961d8e5b8de
LeeKangh22 <dlrkdgml1998@dongguk.edu>
2021-09-11 14:27:34 +0900

    9/11 solved by Leekh

--- Level2/H-index.py
+++ Level2/H-index.py
@@ -2,20 +2,21 @@ citations = [3, 0, 6, 1, 5]
 
 def solution(citations):
     answer = 0
-    temp = []
     citations.sort()
-    count = 0
     h_temp = []
+    if len(citations) == 1:
+        return 1
     for i in range(0, len(citations)):
-        print(citations)
-        h = citations[i]
-        count = len(citations)
-        if count >= h:
-            h_temp.append(h)
-            citations.pop(0)
+        if citations[i] == 0:
+            h_temp.append(0)
         else:
-            break
+            for j in range(0, citations[i]):
+                if j + 1 <= len(citations) - i:
+                    h_temp.append(j + 1) 
     answer = max(h_temp)
-    
     return answer
 print(solution(citations))
+
+
+    
+    
