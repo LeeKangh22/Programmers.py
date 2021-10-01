a673020bf2fdcca4c68d6843b4e0518de4922839
LeeKangh22 <dlrkdgml1998@dongguk.edu>
2021-10-01 15:00:09 +0900

    10/1 solved by Leekh

---
+++ Level2/올바른 괄호.py
@@ -0,0 +1,14 @@
+def solution(s):
+    temp = []
+    if len(s) == 1 or len(s) == 0:
+        return False
+    else:
+        for i in range(0, len(s)):
+            temp.append(s[i])
+            if len(temp) >= 2 and temp[-1] == ')' and temp[-2] == '(':
+                temp.pop()
+                temp.pop()
+    if len(temp) == 0:
+        return True
+    else:
+        return False
