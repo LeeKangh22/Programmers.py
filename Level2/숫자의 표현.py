5f255393c5dcfb7677c99d5f85f4bf3efd77620d
LeeKangh22 <dlrkdgml1998@dongguk.edu>
2021-10-01 14:28:06 +0900

    10/ sovled by Leekh

--- Level2/숫자의 표현.py
+++ Level2/숫자의 표현.py
@@ -1,4 +1,5 @@
 n = 15
+'''
 def solution(n):
     answer = 0
     sum = 0
@@ -12,4 +13,12 @@ def solution(n):
         sum = 0
     return answer + 1
 #시간 초과 코드
+'''
+def solution(n):
+    answer = 0
+    for i in range(1, n + 1):
+        if n % i == 0 and i % 2 == 1:
+            answer += 1
+    return answer 
+# 약수 중 홀수인 수의 개수와 같다..왜..?
 print(solution(n))
