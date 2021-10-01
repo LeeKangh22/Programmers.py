9225637af9de5a730b1cf2a8fba52cec6b343ec0
LeeKangh22 <dlrkdgml1998@dongguk.edu>
2021-10-01 13:18:12 +0900

    10/1 solved by Leekh

---
+++ Level2/피보나치 수.py
@@ -0,0 +1,11 @@
+n=10000
+def solution(n):
+    fibo = []
+    fibo.append(0)
+    fibo.append(1)
+    for i in range(2, n):
+        fibo.append(fibo[i - 2] + fibo[i - 1])
+    return (fibo[n - 2] + fibo[n - 1]) % 1234567
+    
+    
+print(solution(n))
