51f285fee6663f98458c25e5f68b9b02d8f0d6bd
LeeKangh22 <dlrkdgml1998@dongguk.edu>
2021-10-01 15:12:52 +0900

    10/1 solved by Leekh

--- Level2/괄호 회전하기.py
+++ Level2/괄호 회전하기.py
@@ -1,56 +1,30 @@
-s = "}]()[{"
+s = "}}}"
 def solution(s):
     answer = 0
     temp = []
-    for j in range(0, len(s)):
-        temp.append(s[j])
-    for j in temp:
-        if j == '(':
-            for k in range(temp.index(j), len(s)):
-                if temp[k] == ')':
-                    temp.pop(temp.index(j))
-                    temp.pop(k)
-                    break
-        elif j == '{':
-            for k in range(temp.index(j), len(s)):
-                if temp[k] == '}':
-                    temp.pop(temp.index(j))
-                    temp.pop(k)
-                    break   
-        elif j == '[':
-            for k in range(temp.index(j), len(s)):
-                if temp[k] == ']':
-                    temp.pop(temp.index(j))
-                    temp.pop(k)
-                    break
-    if len(temp) == 0:
-        answer += 1
-    for i in range(1, len(s)):
-        temp = []
+    for i in s:
         s += s[0]
         s = s[1:]
         for j in range(0, len(s)):
             temp.append(s[j])
-        for j in temp:
-            if j == '(':
-                for k in range(temp.index(j), len(s)):
-                    if temp[k] == ')':
-                        temp.pop(temp.index(j))
-                        temp.pop(k)
-                        break
-            elif j == '{':
-                for k in range(temp.index(j), len(s)):
-                    if temp[k] == '}':
-                        temp.pop(temp.index(j))
-                        temp.pop(k)
-                        break   
-            elif j == '[':
-                for k in range(temp.index(j), len(s)):
-                    if temp[k] == ']':
-                        temp.pop(temp.index(j))
-                        temp.pop(k)
-                        break
+            if len(temp) >= 2 and temp[-2] == '[':
+                if temp[-1] == ']':
+                    temp.pop()
+                    temp.pop()
+            if len(temp) >= 2 and temp[-2] == '{':
+                if temp[-1] == '}':
+                    temp.pop()
+                    temp.pop()
+            if len(temp) >= 2 and temp[-2] == '(':
+                if temp[-1] == ')':
+                    temp.pop()
+                    temp.pop()
         if len(temp) == 0:
             answer += 1
+            temp = []
+        else:
+            temp = []
+            
+    
     return answer
 print(solution(s))
